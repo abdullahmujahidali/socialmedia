4958163df18ba41dee914a4a6d176e7d3d340870
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-09-30 12:39:27 +0500

    added jwt

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://abdullah:abdullah@cluster0.oeocc.mongodb.net/test?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://abdullah:abdullah@cluster0.oeocc.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "f17@QBUA"
 }
