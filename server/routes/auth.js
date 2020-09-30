ffdd5e843d079323bd8f107c282e42dcfa2e9774
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-09-30 12:56:00 +0500

    middleware to verify token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,6 +5,10 @@ const User = mongoose.model("User")
 const bcrypt = require("bcryptjs");
 const jwt= require("jsonwebtoken");
 const {JWT_SECRET}= require("../../keys")
+const requireLogin = require("../middleware/requireLogin")
+router.get("/protected",requireLogin,(req,res)=>{
+    res.send("hello user");
+})
 
 router.post("/signup", (req, res) => {
     const { name, email, password } = req.body
