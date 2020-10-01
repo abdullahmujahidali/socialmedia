a13455a81301405276107ac4302f73c30d78d7f1
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-01 06:53:13 +0500

    Add all posts route

--- server/routes/post.js
+++ server/routes/post.js
@@ -7,12 +7,24 @@ const Post = mongoose.model("Post")
 //     res.send("hello user");
 // })
 
+router.get("/allpost",(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")
+    .then(posts=>{
+
+        res.json(posts)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
 router.post("/createpost",requireLogin,(req,res)=>{
     const {title,body}=req.body
     if(!title || !body){
         return res.status(422).json({error:"Please add all fields"})
     }
-    req.user.password = undefined
+    req.user.password = undefined 
     const post = new Post({
         title,
         body,
