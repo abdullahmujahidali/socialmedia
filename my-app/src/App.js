7c4b1cf38e54cbe6323d566db0ae56641205e39a
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-02 19:17:39 +0500

    Added Browser Router and Other Routing

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import NavBar from "./components/Navbar"
+import {BrowserRouter, Route} from "react-router-dom"
+import "./App.css"
+import Home from "./components/screens/Home"
+import Signin from "./components/screens/Login"
+import Profile from "./components/screens/Profile"
+import Signup from "./components/screens/Signup"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <NavBar />
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route path="/signin">
+      <Signin />
+    </Route>
+    <Route path="/signup">
+      <Signup />
+    </Route>
+    <Route path="/profile">
+      <Profile />
+    </Route>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
