86ec194a5876a2eb14849a80ea227bc0bc4a5c7a
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-02 21:43:05 +0500

    UI created for Home

--- my-app/src/components/screens/Home.js
+++ my-app/src/components/screens/Home.js
@@ -2,7 +2,45 @@ import React from "react"
 
 const Home =()=>{
 return (
-    <h1>HOME </h1>
+    <div className="home">
+        <div className="card home-card">
+            <h5>Abdullah Mujahid</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1461301214746-1e109215d6d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="post"></img>
+            </div>
+            <div className="card-content">
+            <i class="material-icons" style={{color:"red"}}>whatshot</i>
+                <h4>Title</h4>
+                <p>This is an amazing post</p>
+                <input type="text" placeholder="Add a comment" />
+            </div>
+        </div>
+        <div className="card home-card">
+            <h5>Abdullah Mujahid</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1461301214746-1e109215d6d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="post"></img>
+            </div>
+            <div className="card-content">
+            <i class="material-icons" style={{color:"red"}}>whatshot</i>
+                <h4>Title</h4>
+                <p>This is an amazing post</p>
+                <input type="text" placeholder="Add a comment" />
+            </div>
+        </div>
+        <div className="card home-card">
+            <h5>Abdullah Mujahid</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1461301214746-1e109215d6d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="post"></img>
+            </div>
+            <div className="card-content">
+            <i class="material-icons" style={{color:"red"}}>whatshot</i>
+                <h4>Title</h4>
+                <p>This is an amazing post</p>
+                <input type="text" placeholder="Add a comment" />
+            </div>
+        </div>
+        
+    </div>
 )
 }
 
