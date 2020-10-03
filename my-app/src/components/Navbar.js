0cffa316571b5d3e3a96b33d4b639ef304fa41ec
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-03 11:25:08 +0500

    Added Cloudinary for images

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,19 +1,32 @@
-import React from "react"
-import {Link} from "react-router-dom"
-const NavBar = ()=>{
-    return (
-        <nav>
+import React, { Fragment } from "react"
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <Fragment>
+    
+      <nav>
         <div className="nav-wrapper black ">
-          <Link to="/" className="brand-logo left ">Snap 4 Everyone</Link>
-          <ul id="nav-mobile" className="right">
+          <Link to="/" className="brand-logo  ">Snap 4 Everyone</Link>
+          <Link to="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
+          <ul className="right hide-on-med-and-down">
             <li><Link to="/signin">Login</Link></li>
             <li><Link to="/signup">Sign Up</Link></li>
             <li><Link to="/profile">Profile</Link></li>
             <li><Link to="/create">Create Post</Link></li>
           </ul>
         </div>
+
       </nav>
-    )
+      <ul className="sidenav" id="mobile-demo">
+        <li><Link to="/signin">Login</Link></li>
+        <li><Link to="/signup">Sign Up</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/create">Create Post</Link></li>
+      </ul>
+   
+    </Fragment>
+  )
 }
 
 export default NavBar
--- my-app/src/components/screens/CreatePost.js
+++ my-app/src/components/screens/CreatePost.js
@@ -1,6 +1,57 @@
-import React from "react"
-
+import React,{useState} from "react"
+import {useHistory} from "react-router-dom"
+import M from "materialize-css"
 const CreatePost = () => {
+    const history=useHistory()
+    const [title,setTitle]= useState("")
+    const [body,setBody]= useState("")
+    const [image,setImage]=useState("")
+    const [url,setUrl]=useState("")
+    const postDetails=()=>{
+        const data=new FormData()
+        
+        data.append("file",image)
+        data.append("upload_preset","snap-everyone")
+        data.append("cloud_name","imagedirectory")
+        fetch("https://api.cloudinary.com/v1_1/imagedirectory/image/upload",{
+        method:"post",
+        body:data
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        setUrl(data.url)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    fetch('/createpost',{
+        method:"post",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            title,
+            body,
+            pic:url
+        })
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        if(data.error){
+            M.toast({html:data.error, classes:"#004d40 teal darken-4"})
+        }
+        else{
+            M.toast({html: "Post created", classes:"#03a9f4 light-blue"})
+            history.push("/")
+        }
+     }).catch(err=>{
+         console.log(err)
+     })
+}
+
+
+    
+
     return (
         <div className="card input-field"
         style={{
@@ -10,19 +61,22 @@ const CreatePost = () => {
             textAlign: "center"
         }}
         >
-            <input type="text" placeholder="Title"></input>
-            <input type="text" placeholder="Body"></input>
+            <input type="text" placeholder="Title" value ={title} onChange={(e)=>setTitle(e.target.value)}></input>
+            <input type="text" placeholder="Body" value ={body} onChange={(e)=>setBody(e.target.value)}></input>
             <div className="file-field input-field">
                 <div className="btn #b71c1c red darken-4">
                     <span>Upload image</span>
-                    <input type="file" />
+                    <input type="file"  onChange={(e)=>setImage(e.target.files[0])}/>
                 </div>
                 <div className="file-path-wrapper">
                     <input className="file-path validate" type="text" />
                 </div>
             </div>
-            <button className="btn waves-effect waves-light #b71c1c red darken-4">Submit Post
-             </button>
+            <button className="btn waves-effect waves-light #b71c1c red darken-4"
+            onClick={()=>postDetails()}
+            >Submit Post
+             
+            </button>
         </div>
     )
 }
