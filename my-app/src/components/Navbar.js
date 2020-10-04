70bf3b19df4b24b19ea3ee5a36e49e9241233616
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-04 19:47:20 +0500

    Added Logout function

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,14 +1,24 @@
 import React, { useContext,Fragment } from "react"
-import { Link } from "react-router-dom"
+import { Link ,useHistory} from "react-router-dom"
 import {UserContext} from "../App"
 const NavBar = () => {
   const {state,dispatch}=useContext(UserContext)
+  const history=useHistory()
   const renderList=()=>{
     if(state){
       return [
         <Fragment>
         <li><Link to="/profile">Profile</Link></li>
         <li><Link to="/create">Create Post</Link></li>
+        <button className="btn waves-effect waves-light #b71c1c red darken-4"
+              onClick={()=>{
+                localStorage.clear()
+                dispatch({type:"CLEAR"})
+                history.push("/signin")
+              }}  
+             >LOG OUT
+             </button>
+        
         </Fragment>
       ]
     }
@@ -17,6 +27,7 @@ const NavBar = () => {
       <Fragment>
       <li><Link to="/signin">Login</Link></li>
       <li><Link to="/signup">Sign Up</Link></li>
+      
       </Fragment>
       ]
     }
