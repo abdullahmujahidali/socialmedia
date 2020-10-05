afc30e325d269b3c76f20768d19ac088759d1589
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2020-10-05 09:00:53 +0500

    Added logic for liking and unliking a post on the client side of the application

--- my-app/src/components/screens/Home.js
+++ my-app/src/components/screens/Home.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react"
-
+import React, { useState, useEffect,useContext } from "react"
+import {UserContext} from "../../App"
 const Home = () => {
     const [data, setData] = useState([])
+    const {state,dispatch}=useContext(UserContext)
     useEffect(() => {
         fetch("/allpost", {
             headers: {
@@ -9,9 +10,61 @@ const Home = () => {
             }
         }).then(res => res.json())
             .then(result => {
+               // console.log(result)
                 setData(result)
             })
     }, [])
+    const likePost=(id)=>{
+        fetch("/like",{
+            method:"put",
+            headers:{
+                "Content-type": "application/json",
+                "Authorization": "Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData=data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }
+                else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const unlikePost=(id)=>{
+        fetch("/unlike",{
+            method:"put",
+            headers:{
+                "Content-type": "application/json",
+                "Authorization": "Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            // console.log(result)
+            const newData=data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }
+                else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
     return (
         <div className="home">
             {
@@ -24,6 +77,13 @@ const Home = () => {
                             </div>
                             <div className="card-content">
                                 <i className="material-icons" style={{ color: "red" }}>whatshot</i>
+                                {item.likes.includes(state._id)
+                                ?  <i className="material-icons" onClick={()=>{unlikePost(item._id)}}>star</i>
+                                :
+                                <i className="material-icons" style={{color:"red"}} onClick={()=>{likePost(item._id)}}>star_border</i>
+                                }
+                                
+                                <h6>{item.likes.length} stars</h6>
                                 <h4>{item.title}</h4>
                                 <p>{item.body}</p>
                                 <input type="text" placeholder="Add a comment" />
